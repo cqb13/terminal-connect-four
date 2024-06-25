2d72005175e57d5f41a77a5b714609788a75386b
cqb13 <cqb13.main@gmail.com>
2024-06-25 12:20:33 -0400

    draw board

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -3,6 +3,10 @@ mod singleplayer;
 
 use self::multiplayer::play_multiplayer;
 use self::singleplayer::play_singleplayer;
+use crate::display::colors::Color;
+
+const GAME_BOARD_WIDTH: i32 = 7;
+const GAME_BOARD_HEIGHT: i32 = 6;
 
 pub enum GameMode {
     SinglePlayer,
@@ -23,6 +27,154 @@ impl Player {
             Player::Computer => "Computer",
         }
         .to_string()
+    }
+
+    pub fn color(&self) -> Piece {
+        match self {
+            Player::PlayerOne => Piece::Red,
+            Player::PlayerTwo | Player::Computer => Piece::Yellow,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum Piece {
+    Red,
+    Yellow,
+    None,
+}
+
+impl Piece {
+    pub fn to_string(&self) -> String {
+        match self {
+            Piece::Red => "R",
+            Piece::Yellow => "Y",
+            Piece::None => "E",
+        }
+        .to_string()
+    }
+
+    pub fn to_symbol(&self) -> String {
+        match self {
+            Piece::Red | Piece::Yellow => "⏺",
+            Piece::None => " ",
+        }
+        .to_string()
+    }
+
+    pub fn ansi_color(&self) -> String {
+        match self {
+            Piece::Red => Color::Red,
+            Piece::Yellow => Color::Yellow,
+            Piece::None => Color::White,
+        }
+        .to_ansi_color_code()
+        .to_string()
+    }
+}
+
+impl PartialEq for Piece {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Piece::Red, Piece::Red) => true,
+            (Piece::Yellow, Piece::Yellow) => true,
+            (Piece::None, Piece::None) => true,
+            _ => false,
+        }
+    }
+}
+
+pub struct GameState {
+    current_player: Player,
+    game_mode: GameMode,
+    game_board: Vec<Vec<Piece>>,
+    width: i32,
+    height: i32,
+}
+
+impl GameState {
+    pub fn new(game_mode: GameMode) -> GameState {
+        let game_board =
+            vec![vec![Piece::None; GAME_BOARD_WIDTH as usize]; GAME_BOARD_HEIGHT as usize];
+
+        GameState {
+            current_player: Player::PlayerOne,
+            game_mode,
+            game_board,
+            width: GAME_BOARD_WIDTH,
+            height: GAME_BOARD_HEIGHT,
+        }
+    }
+
+    pub fn drop_piece_in_column(&mut self, column: i32) -> Result<(), String> {
+        if column > self.width || column < 1 {
+            return Err("Column does not exist on boar".to_string());
+        }
+
+        if self.game_board[0][column as usize] != Piece::None {
+            return Err("The selected column is full".to_string());
+        }
+
+        // finds the first open spot on the column
+        for row in 0..self.height {
+            if self.game_board[row as usize][column as usize - 1] != Piece::None {
+                self.game_board[row as usize - 1][column as usize - 1] =
+                    self.current_player.color();
+                return Ok(());
+            }
+        }
+
+        // the row is empty, place piece at bottom
+        self.game_board[self.height as usize - 1][column as usize - 1] =
+            self.current_player.color();
+
+        Ok(())
+    }
+
+    pub fn switch_player(&mut self) {
+        let opposite_player = match self.current_player {
+            Player::PlayerOne => match self.game_mode {
+                GameMode::Multiplayer => Player::PlayerTwo,
+                GameMode::SinglePlayer => Player::Computer,
+            },
+            _ => Player::PlayerOne,
+        };
+
+        self.current_player = opposite_player
+    }
+
+    pub fn debug(&self) {
+        for row in &self.game_board {
+            for piece in row {
+                print!(" {} ", piece.to_string())
+            }
+            println!()
+        }
+    }
+
+    pub fn draw_board(&self) {
+        for row in &self.game_board {
+            print!("+");
+            for _ in 0..self.width {
+                print!("---+");
+            }
+            println!();
+            print!("|");
+            for piece in row {
+                print!(
+                    " {}{}{} |",
+                    piece.ansi_color(),
+                    piece.to_symbol(),
+                    Color::Clear.to_ansi_color_code()
+                );
+            }
+            println!();
+        }
+        print!("+");
+        for _ in 0..self.width {
+            print!("---+");
+        }
+        println!();
     }
 }
 
--- src/game/singleplayer.rs
+++ src/game/singleplayer.rs
@@ -1,6 +1,14 @@
-use super::GameResult;
+use super::{GameResult, GameState, GameMode};
 
 pub fn play_singleplayer() -> GameResult {
-    println!("singleplayer");
+    let mut game_state = GameState::new(GameMode::SinglePlayer);
+
+    game_state.drop_piece_in_column(1).expect("Fail drop 1");
+    game_state.switch_player();
+    game_state.drop_piece_in_column(2).expect("Fail drop 2");
+    game_state.switch_player();
+    game_state.drop_piece_in_column(2).expect("Fail drop 3");
+    game_state.draw_board();
+
     GameResult::new()
 }
